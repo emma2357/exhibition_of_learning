8d36140843c372bb824404ab303999e39f825043
emma2357 <capaldi.ei@gmail.com>
2024-05-04 14:44:41 -0400

    connection string

--- knexfile.js
+++ knexfile.js
@@ -26,12 +26,12 @@ module.exports = {
   production: {
     client: "pg",
     connection: {
-      host: process.env.DB_HOST,
-      port: 5432,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      //conectionString: process.env.DATABASE_URL2,
+      //host: process.env.DB_HOST,
+      //port: 5432,
+      //user: process.env.DB_USER,
+      //password: process.env.DB_PASSWORD,
+      //database: process.env.DB_DATABASE,
+      connectionString: process.env.DATABASE_URL,
       ssl: {rejectUnauthorized: false},
     },
     migrations: {
